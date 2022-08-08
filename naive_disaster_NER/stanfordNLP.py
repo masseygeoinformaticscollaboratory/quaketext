49aec0bf29e3ab7983e3292f2255742b24e4e4ba
sophiexander <sophie3606@gmail.com>
2022-08-08 17:48:23 +1200

    accesng lemma and entities from doc sentences

--- naive_disaster_NER/stanfordNLP.py
+++ naive_disaster_NER/stanfordNLP.py
@@ -3,12 +3,23 @@ import stanza
 # nlp = stanza.Pipeline('en') # initialize English neural pipeline
 # doc = nlp("Barack Obama was born in Hawaii.") # run annotation over a sentence
 
-nlp = stanza.Pipeline(lang='en', processors='tokenize', tokenize_no_ssplit=True)
-doc = nlp("He broke into song as the buildings in Queen Street collapsed around him and the 100 sheep died.")
+nlp = stanza.Pipeline(lang='en', processors='tokenize, mwt, pos, lemma, ner', tokenize_no_ssplit=True)
+# need to specify what processors to use in order to analyze the text
+
+# doc = nlp("He broke into song as the buildings in Queen Street collapsed around him and the 100 sheep died.")
+
+doc = nlp("Chris Manning teaches at Stanford University. He lives in the Bay Area.")
+
 
 # print(doc)
-# print(doc.entities) # this prints out all the words that were triggered by NER
+print(doc.entities) # this prints out all the words that were triggered by NER
+
+disasterNERlist = ["break", "collapse", "lost", "kill", "die"]
 
 for i, sentence in enumerate(doc.sentences):
-    print(f'====== Sentence {i+1} tokens =======')
-    print(*[f'id: {token.id}\ttext: {token.text}' for token in sentence.tokens], sep='\n')
+    print(f'====== Sentence words =======')
+    print(*[f'word: {word.text}\tlemma: {word.lemma}' for word in sentence.words], sep='\n')
+    print(f'====== Sentence ents =======')
+    print(*[f'word: {ent.text}\tlemma: {ent.type}' for ent in sentence.ents], sep='\n')
+    print(f'====== Sentence tokens =======')
+    print(*[f'token: {token.text}\tner: {token.ner}' for token in sentence.tokens], sep='\n')
