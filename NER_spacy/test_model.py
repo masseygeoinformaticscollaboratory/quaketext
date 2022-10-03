62c98292c2eba3675d7fb4b11037cded5b75f408
sophiexander <sophie3606@gmail.com>
2022-10-03 17:40:31 +1300

    output files

--- NER_spacy/test_model.py
+++ NER_spacy/test_model.py
@@ -8,9 +8,9 @@ nlp = spacy.load("./output/model-last/") #load the model
 
 # text = "Chile Earthquake: 5 Dead, Several Seriously Injured http://t.co/9v1Xr25bNg"
 
-# text = "RT @TIME: See India's rescue operations in the villages most affected by Nepal's deadly earthquake http://t.co/Tcirp7hceR http://t.co/LCMDoâ€¦"
+text = "RT @TIME: See India's rescue operations in the villages most affected by Nepal's deadly earthquake http://t.co/Tcirp7hceR http://t.co/LCMDoâ€¦"
 
-text = "Over 330 peoples are killed in #Earthquake in #Baluchistan. Many are still missing. #Pakistan"
+# text = "Over 330 peoples are killed in #Earthquake in #Baluchistan. Many are still missing. #Pakistan"
 
 doc = nlp(text)
 print(nlp.pipe_names)
