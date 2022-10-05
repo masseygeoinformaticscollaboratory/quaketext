29593f08171229e3ff784a79840e391eef42b8dd
sophiexander <sophie3606@gmail.com>
2022-10-05 16:05:11 +1300

    make one single combined csv file for LT and MT

--- CSVtoJSONcode/lighttag_json_reformat.py
+++ CSVtoJSONcode/lighttag_json_reformat.py
@@ -3,32 +3,6 @@ from __future__ import annotations
 import csv
 import json
 
-MIN_THRESHOLD = 3
-
-
-
-def save_tweet_tag_count(instance_dict):
-
-    # print(instance_dict)
-    # print("from function")
-
-    for i in instance_dict:
-        if i != 'tweetId' and i != 'tweetText':
-            # print(i)
-
-            instance = instance_dict[i][0]
-            label = instance_dict[i][1]
-            count = str(instance_dict[i][2])
-            start = str(instance_dict[i][3])
-            end = str(instance_dict[i][4])
-
-            if(start == end):
-                print("here", label)
-            
-
-            csv_tag_count_file.write(instance_dict['tweetId'] + "\t" + label + "\t" + instance + "\t"+ count + "\t"+ start + "\t"+ end + "\t"+  instance_dict['tweetText'] + "\n")
-
-
 # ----------------------------------------------------------------------------------
 # output from convertCSVtoJSON - in this case mt_combined.json
 light_tag_json_file_path = "lighttag-800-tweets-annotated.json" # input('Enter the absolute path of the INPUT JSON file: ')
@@ -36,8 +10,8 @@ csv_file_path = "light_tag_results.csv" # input('Enter the absolute path of the 
 
 instance_dict = {}
 
-csv_worker_tags_file = open(csv_file_path, 'w', encoding = 'utf-8')
-csv_worker_tags_file.write('AssignmentStatus' + "\t" + 'tweet_id' + "\t" + 'num taggers' + "\t" + 'label' + "\t" + 'instance' +"\t" + 'startOffset' +"\t" + 'endOffset' +"\t" + 'Input.text' +"\t" + 'Answer.taskAnswers' + "\n")
+light_tag_csv_file = open(csv_file_path, 'w', encoding = 'utf-8')
+light_tag_csv_file.write('AssignmentStatus' + "\t" + 'tweetId' + "\t" + 'num taggers' + "\t" + 'label' + "\t" + 'instance' +"\t" + 'start' +"\t" + 'end' +"\t" + 'tweetText' + "\n")
 
 # csv_tag_count_file = open("tagcount.csv", 'w', encoding = 'utf-8')
 # csv_tag_count_file.write('tweetId' + "\t" + 'label' + "\t" + 'instance' + "\t"+ 'count' + "\t"+ 'start' + "\t"+ 'end' + "\t"+'tweetText' + "\n")
@@ -59,110 +33,24 @@ with open(light_tag_json_file_path, encoding = 'utf-8') as json_file_handler:
             for tag in tweet['annotations']:
                 # print(tag)
 
+                splice = tweet['content'][tag['start']:tag['end']]
+                if(tag['value']!= splice):
+                    print("tag value", tag['value'])
+                    print("splice" , splice)
+
                 status = True
 
                 if (tag['correct'] == False):
                     status = False
 
-                csv_worker_tags_file.write(str(status) + "\t" + tweet['metadata']['tweet_id'] + "\t" + str(len(tag['annotated_by'])) + "\t" + tag['tag'] + "\t" + tag['value'] + "\t" + str(tag['start']) + "\t" + str(tag['end']) + "\t" + tweet['content'] + "\t" + "\n")
+                light_tag_csv_file.write(str(status) + "\t" + tweet['metadata']['tweet_id'] + "\t" + str(len(tag['annotated_by'])) + "\t" + tag['tag'] + "\t" + tag['value'] + "\t" + str(tag['start']) + "\t" + str(tag['end']) + "\t" + tweet['content'] + "\n")
 
 
             count += 1
 
-        # if count == 0:
-        #     instance_dict.update({"tweetId" : i['Input.id']})
-        #     instance_dict.update({"tweetText" : i['Input.text']})
-        #     # print(instance_dict)
-        # elif instance_dict["tweetId"] != i['Input.id']:
-        #     # reached a new tweet id file row - change dictionary
-            
-        #     # writing to tag_count file once all the tags for that tweet have been read
-        #     # save_tweet_tag_count(instance_dict)
-
-        #     instance_dict.clear()
-        #     instance_dict.update({"tweetId" : i['Input.id']})
-        #     instance_dict.update({"tweetText" : i['Input.text']})
-        #     # print(instance_dict)
-        # # else:
-        # #     # tweet ID is the same use existing counts
-
-        # # if the MT tag is approved  
-        # if i['AssignmentStatus'] == "Approved":
-            
-        #     task_object = json.loads(i['Answer.taskAnswers'])
-
-        #     name = 'annotation-tweet-id-'+ i['Input.id']
-
-        #     entitiesPresent = False
-
-        #     # https://stackoverflow.com/questions/24708634/python-and-json-typeerror-list-indices-must-be-integers-not-str
-        #     for tag in task_object[0][name]['entities']:
-        #         # for all the entities if there are any
-        #         entitiesPresent = True
-                
-        #         start = tag['startOffset']
-        #         end = tag['endOffset']
-
-        #         # https://www.pythoncentral.io/cutting-and-slicing-strings-in-python/
-        #         instance = i['Input.text'][start:end]
-
-        #         # removing edge whitespace from the tag
-        #         letter_count = 0
-        #         string_length = len(instance)
-
-        #         for letter in instance:
-        #             if letter == ' ':
-
-        #                 if(letter_count == 0):
-        #                     # print("inbefore-" + instance + "-")
-        #                     instance = instance.lstrip()
-        #                     # print("inafter-" + instance + "-")
-        #                     start = start + 1
-        #                     string_length = string_length = 1
-                        
-        #                 elif(letter_count == string_length - 1):
-        #                     # print("before-" + instance + "-")
-        #                     instance = instance.rstrip()
-        #                     # print("after-" + instance + "-")
-        #                     end = end - 1
-                    
-        #             letter_count += 1
-                
-        #         # -------------------------------------------------
-                
-        #         if start != end:
-        #             # if in the case that an empty space was tagged
-        #             csv_worker_tags_file.write(i['AssignmentStatus'] + "\t" + i['Input.id'] + "\t" + i['WorkerId'] + "\t" + tag['label'] + "\t" + instance + "\t" + str(start) + "\t" + str(end) + "\t" + i['Input.text'] + "\t" + i['Answer.taskAnswers'] + "\n")
-        #             count = count + 1
-
-        #             instance_count = 1
-        #             key = instance+"-"+tag['label']+"-"+str(start)+"-"+str(end)
-
-        #             curr_dict_state = instance_dict.keys()
-
-        #             if key in curr_dict_state:
-        #                 # if it exists in the instance_dict add one to the count
-        #                 instance_count = instance_dict[key][2] + 1
-
-        #             instance_dict.update({key:[instance, tag['label'], instance_count, start, end]})
-
-        #     if entitiesPresent == False:
-        #         csv_worker_tags_file.write(i['AssignmentStatus'] + "\t" + i['Input.id'] + "\t" + i['WorkerId'] + "\t" + "\t" + "\t" + "\t" + "\t" + i['Input.text'] + "\t" + i['Answer.taskAnswers'] + "\n")
-
-        #     entitiesPresent = False
-            
-
-        # # only look at accepted tags from approved tasks
-        # # extracting of the words - new line for each?
-        # else:
-        #     csv_worker_tags_file.write(i['AssignmentStatus'] + "\t" + i['Input.id'] + "\t" + i['WorkerId'] + "\n")
-
-
-    # last save for last tweet in file
-    save_tweet_tag_count(instance_dict)
     print(count)
 
-csv_worker_tags_file.close()
+light_tag_csv_file.close()
 # csv_tag_count_file.close()
 
 
@@ -183,22 +71,22 @@ with open("light_tag_results.csv", 'r', encoding = 'utf-8') as csv_file:
     total_tweet_count = 0
 
     for rows in csv_reader:
-        print(rows)
+        # print(rows)
 
         if(rows['AssignmentStatus'] != "False"):
                 
             if first == True:
              # if the first row in the file, that is the current
-                currentId = rows['tweet_id']
-                currentText = rows['Input.text']
+                currentId = rows['tweetId']
+                currentText = rows['tweetText']
                 light_dict[currentId] = ({"tweetId":currentId, "content": currentText,})
                 light_dict[currentId]["annotations"] = []
                 first = False 
                 total_tweet_count += 1      
-            elif currentId != rows['tweet_id']:
+            elif currentId != rows['tweetId']:
             # if the ids do not match then all the annotations for the previous tweet have been read save to dictionary and change current it and text, and clear annotation list for new tweet 
-                currentId = rows['tweet_id']
-                currentText = rows['Input.text']
+                currentId = rows['tweetId']
+                currentText = rows['tweetText']
                 light_dict[currentId] = ({"tweetId":currentId, "content": currentText,})
                 light_dict[currentId]["annotations"] = []
 
@@ -216,12 +104,12 @@ with open("light_tag_results.csv", 'r', encoding = 'utf-8') as csv_file:
             # print("curr state")
             # print(current_annotation_state)
 
-            light_dict[currentId]["annotations"] = (current_annotation_state + [{"tag": rows['label'],"count": rows['num taggers'],"value": rows['instance'], "start": rows['startOffset'],"end": rows['endOffset']}])
+            light_dict[currentId]["annotations"] = (current_annotation_state + [{"tag": rows['label'],"count": rows['num taggers'],"value": rows['instance'], "start": rows['start'],"end": rows['end']}])
 
 
     print("total tweet count = " + str(total_tweet_count))
 
-            
+csv_file.close()
 
 # # add dictionary to json file
 json_lightTag_format = "lighttag_finaltags.json"
@@ -229,3 +117,67 @@ with open(json_lightTag_format, 'w', encoding = 'utf-8') as json_file:
     json_file.write(json.dumps(light_dict, indent = 4))
 
 
+
+
+# # create a joined CSV file for both lighttag and MT data
+def create_joined_csv_for_LT_MT():
+
+    final_tags_combined_csv = open("final_tags_combined_LT_MT.csv", 'w', encoding = 'utf-8')
+    final_tags_combined_csv.write('source' + "\t" + 'tweetId' + "\t" + 'label' + "\t" + 'instance' +"\t" + 'start' +"\t" + 'end' +"\t" + 'tweetText'  + "\n")
+    num = 0
+    
+    # lighttag data-------------------------------------------------------------------
+    with open("light_tag_results.csv", 'r', encoding = 'utf-8') as LT_csv_file:
+
+        LT_csv_reader = csv.DictReader(LT_csv_file, delimiter='\t')
+
+        for LT_rows in LT_csv_reader:
+        
+
+            if(LT_rows['AssignmentStatus'] == "True"):
+           
+                # if(num == 173):
+                #     print (LT_rows)
+                # if(num == 174):
+                #     print (LT_rows)
+
+                source = "lightTag"
+                id = LT_rows['tweetId']
+                label = LT_rows['label']
+                instance = LT_rows['instance']
+                start = str(LT_rows['start'])
+                end = str(LT_rows['end'])
+                tweet = LT_rows['tweetText']
+
+                final_tags_combined_csv.write(source + "\t" + id + "\t" + label + "\t" +    instance + "\t" + start + "\t" + end + "\t" + tweet + "\n")
+
+                # print(source)
+                num += 1
+                # print(num)
+
+    # Mechanical Turk data----------------------------------------------------------
+    with open("tagcount.csv", 'r', encoding = 'utf-8') as MT_csv_file:
+
+        MT_csv_reader = csv.DictReader(MT_csv_file, delimiter='\t')
+
+        for MT_rows in MT_csv_reader:
+        
+
+            if(int(MT_rows['count']) > 3):
+           
+                if(num < 17):
+                    print (MT_rows)
+
+                source = "MechanicalTurk"
+                id = MT_rows['tweetId']
+                label = MT_rows['label']
+                instance = MT_rows['instance']
+                start = str(MT_rows['start'])
+                end = str(MT_rows['end'])
+                tweet = MT_rows['tweetText']
+
+                final_tags_combined_csv.write(source + "\t" + id + "\t" + label + "\t" +    instance + "\t" + start + "\t" + end + "\t" + tweet + "\n")
+
+    
+
+create_joined_csv_for_LT_MT()
