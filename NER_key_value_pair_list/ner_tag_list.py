ec5625f4a9cda212138193b6e9be37ac4a2af52d
sophiexander <sophie3606@gmail.com>
2022-10-07 18:26:27 +1300

    key value method calculating pos/neg values

--- NER_key_value_pair_list/ner_tag_list.py
+++ NER_key_value_pair_list/ner_tag_list.py
@@ -1,76 +1,113 @@
 import json
 import csv
+import re
 
-impact_list = ['cancellation','closure','collapse','damage','damaged','dead','death','delay','die','disruption','destroy','destroyed','down','injured','injury','lose','loss','loss of life','malfunction','miss work','missing','no access','obstruction','rescue','stranded','theft','trapped', 'kill']
+impact_list = ['cancellation','closure','collapse','damage','dead','death','delay','die','disruption','destroy','down','injured','injury','injuries','lose','loss','loss of life','malfunction','miss work','missing','no access','obstruction','rescue','stranded','theft','trap', 'kill']
 # missing capitalization of some impact instances
   
 # Opening JSON file
-f = open('../CSVtoJSONcode/finaltags.json')
+MT_finaltags_file = open('../CSVtoJSONcode/finaltags.json')
 
-test_csv_file = "C:/Users/sophi/Documents/Uni/QuakeText_Project_Files/MT_disaster_impact_input_3.csv"
+json_file_path = "../CSVtoJSONcode/mt_combined.json"
 
-test_data_dict = {}
-# https://stackoverflow.com/questions/17912307/u-ufeff-in-python-string#:~:text=The%20Unicode%20character%20U%2BFEFF,will%20remove%20it%20for%20you.
-with open(test_csv_file, "r", encoding = 'utf-8-sig') as csv_file_handler:
-    csv_reader = csv.DictReader(csv_file_handler)
- 
+impact_list_dict = {}
+
+
+with open(json_file_path, encoding = 'utf-8') as json_handler:
+    
+    data = json.load(json_handler)
+
     count = 0
  
-    for rows in csv_reader:
-        # print(rows)
- 
-        key = rows['id']
-        # create the dictionary of tweets
-        test_data_dict[key] = rows
+    for i in data.values():
+        print(i['Input.id'])
 
-        count+=1
+        impact_list_dict.update({i['Input.id']:{"id" : i['Input.id'],"text" : i['Input.text']}})
 
-for tweet in test_data_dict:
-    print(test_data_dict[tweet]['text'])
-    test_data_dict[tweet]['tag'] = []
+        # count+=1
+    
+    print(impact_list_dict, count)
+
+for tweet in impact_list_dict:
+    print(impact_list_dict[tweet])
+    impact_list_dict[tweet]['tag'] = []
     for impact in impact_list:
-        if impact in test_data_dict[tweet]['text']:
+        lowercase_tweet = impact_list_dict[tweet]['text']
+
+        # if re.search(impact, impact_list_dict[tweet]['text'], re.IGNORECASE):
+        if impact in lowercase_tweet:
             print("found impact = " + impact)
-            start = test_data_dict[tweet]['text'].find(impact)
-            print(start)
-            end = test_data_dict[tweet]['text'].rfind(impact)
-            print(end)
-            current_tags = test_data_dict[tweet]['tag']
-            current_tags.extend([{'value':impact,'start':str(start)}])
-            test_data_dict[tweet]['tag'] = current_tags
+            # start = impact_list_dict[tweet]['text'].find(impact)
+            start = lowercase_tweet.find(impact)
+            # print(start)
+            # end = impact_list_dict[tweet]['text'].rfind(impact)
+            end = lowercase_tweet.rfind(impact)
+            
+            # print(end)
+            current_tags = impact_list_dict[tweet]['tag']
+            current_tags.extend([{'value':impact,'start':str(start),"found": "null"}])
+            impact_list_dict[tweet]['tag'] = current_tags
             # test_data_dict[tweet]['tag'] = [{'value':impact,'start':str(start)}]
 
     
-with open("test_result.json", 'w', encoding = 'utf-8') as json_file_handler:
-    json_file_handler.write(json.dumps(test_data_dict, indent = 4))
 
 
   
 # returns JSON object as 
 # a dictionary
-data = json.load(f)
+MT_data = json.load(MT_finaltags_file)
 
   
-# Iterating through the json
+# Iterating through the json MT file for gold standard final tags
 # list
 
 print("hello world")
 
+true_pos = 0 # tagged correctly
+false_neg = 0 
+false_pos = 0
+
+found = False
 
 count = 0
-for i in data:
+for tweet in MT_data:
     # print(data[i]['annotations'])
-    impact_only_dict = {}
-    for anno in data[i]['annotations']:
-        if count < 10:
-            # print(anno)
+    # impact_only_dict = {}
+    for anno in MT_data[tweet]['annotations']:
+        if anno['tag'] == "type of impact":
+
+            for i in impact_list_dict[tweet]['tag']:
+                print("inside for ",i)
+                if(i['start'] == anno['start']):
+                    true_pos += 1
+                    i['found'] = "yes"
+                    found = True
+            
+            if(found == False):
+                # word was not found
+                false_neg+= 1
+            
+            found = False
+            print(i)
+            print(anno)
             count = count+1
+
+for tweet in impact_list_dict:
+    for tag in impact_list_dict[tweet]['tag']:
+        if tag['found'] == "null":
+            false_pos+= 1
 
 # TODO need to read in tagged data to compare only impact tags
 # Read in tweets to see if value list is contained within
 # if it is then get start and end and compare with tagged dataset?
 
-    
+print("true pos", true_pos)
+print("false pos", false_pos)
+print("false neg", false_neg)
+
+with open("test_result.json", 'w', encoding = 'utf-8') as json_file_handler:
+    json_file_handler.write(json.dumps(impact_list_dict, indent = 4))
+
 
 # Closing file
-f.close()
+MT_finaltags_file.close()
